d1b89e499322613e64eaa19419ef068a90a3ce9f
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-06-23 21:55:07 +0800

    Update preprocess

--- preprocess.py
+++ preprocess.py
@@ -8,8 +8,19 @@ def utf8enc(file_name):
     with open(file_name, 'wb') as f:
         f.write(text)
 
+def paragraph2line(file_name):
+    with open(file_name, 'r') as f:
+        text = [string.replace('\n', ' ') for string in f.read().split('\n\n')]
+    with open(file_name, 'w') as f:
+        f.write('\n\n'.join(text))
+
+
+def proprocess(file_name):
+    utf8enc(file_name)
+    paragraph2line(file_name)
+
 
 if __name__ == '__main__':
     data_dir = 'data/Gutenberg/txt/'
     txt_files = [data_dir+file_name for file_name in os.listdir(data_dir)]
-    thread_map(utf8enc, txt_files, max_workers=os.cpu_count())
+    thread_map(proprocess, txt_files, max_workers=os.cpu_count())

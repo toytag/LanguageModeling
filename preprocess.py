fb4250a99845244227ceb8c0f2d398a601f2db68
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-06-27 01:38:34 +0800

    data aggregation

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
-import os
+import os, shutil
+from tqdm import tqdm
 from tqdm.contrib.concurrent import thread_map
 
 
@@ -21,7 +22,25 @@ def proprocess(file_name):
     paragraph2line(file_name)
 
 
+def split2files(idx, file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        text = f.read() + '\n'
+    with open(split_file_dir + f'f{idx%50:02d}.txt', 'w') as f:
+        f.write(text)
+
+
+data_dir = 'data/Gutenberg/txt/'
+txt_files = [data_dir + file_name for file_name in os.listdir(data_dir)]
+split_file_dir = 'data/Gutenberg/split/'
+
+
 if __name__ == '__main__':
-    data_dir = 'data/Gutenberg/txt/'
-    txt_files = [data_dir+file_name for file_name in os.listdir(data_dir)]
+
+    if os.path.exists(split_file_dir):
+        shutil.rmtree(split_file_dir)
+    os.mkdir(split_file_dir)
+
     thread_map(proprocess, txt_files, max_workers=os.cpu_count())
+
+    for i, file_name in tqdm(enumerate(txt_files)):
+        split2files(i, file_name)

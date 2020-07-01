b8744885cfc9b3093f43c3029da6cd71eaf2fc74
Zhenzhong Tang <tangzhzh@shanghaitech.edu.cn>
2020-07-02 00:46:11 +0800

    preprocessing update

--- preprocess.py
+++ preprocess.py
@@ -3,44 +3,22 @@ from tqdm import tqdm
 from tqdm.contrib.concurrent import thread_map
 
 
-def utf8enc(file_name):
-    with open(file_name, 'rb') as f:
-        text = f.read().decode('utf-8','ignore').encode("utf-8")
-    with open(file_name, 'wb') as f:
-        f.write(text)
-
-
-def paragraph2line(file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
-        text = [string.replace('\n', ' ') for string in f.read().split('\n\n')]
-    with open(file_name, 'w') as f:
-        f.write('\n\n'.join(text))
-
-
-def proprocess(file_name):
-    utf8enc(file_name)
-    paragraph2line(file_name)
-
-
-def split2files(idx, file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
-        text = f.read() + '\n'
-    with open(split_file_dir + f'f{idx%100:02d}.txt', 'a') as f:
-        f.write(text)
-
-
 data_dir = 'data/Gutenberg/txt/'
-txt_files = [data_dir + file_name for file_name in os.listdir(data_dir)]
+txt_files = [(i, data_dir + file_name) for i, file_name in enumerate(os.listdir(data_dir))]
 split_file_dir = 'data/Gutenberg/split/'
 
 
-if __name__ == '__main__':
+def proprocess(args):
+    idx, file_name = args
+    with open(file_name, 'r', encoding='utf-8', errors='ignore') as f:
+        text = [string.replace('\n', ' ') for string in f.read().split('\n\n')]
+    with open(split_file_dir + f'f{idx%100:02d}.txt', 'a') as f:
+        f.write('\n\n'.join(text) + '\n')
 
+
+if __name__ == '__main__':
     if os.path.exists(split_file_dir):
         shutil.rmtree(split_file_dir)
     os.mkdir(split_file_dir)
 
     thread_map(proprocess, txt_files, max_workers=os.cpu_count())
-
-    for i, file_name in enumerate(tqdm(txt_files)):
-        split2files(i, file_name)
